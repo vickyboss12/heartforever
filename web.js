24ad139a3c058f6c7b34abb46bd9cbcb43c6e313
vickyboss12 <vigneshc2909@gmail.com>
2024-01-27 19:45:28 +0530

    link the js to body

--- web.js
+++ web.js
@@ -5,3 +5,20 @@ fileInput.onchange = () => {
     fileName.textContent = fileInput.files[0].name;
   }
 }
+
+function sendemail(){
+  Email.send({
+Host : "smtp.gmail.com",
+Username : "lokiuniverse@gmail.com",
+Password : "3F9C0B708731CA0A37EAAEC8BA03D6914545",
+To : 'vigneshc2909@gmail.com',
+From : document.getElementById("email").value,
+Subject : "new contact form enquiry",
+Body : "name:"+ document.getElementById("name").value
+       +"<br> email:" + document.getElementById("email").value
+       +"<br> select option:" + document.getElementById("option").value
+       +"<br> subject:" + document.getElementById("message").value
+      }).then(
+message => alert("message send succesfully")
+);
+}
